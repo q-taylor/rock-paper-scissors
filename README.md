2a97ffe25c3ecd50dbc7604c92e18ed89f94f126
quinn <q.taylor@gmail.com>
2023-01-02 15:26:12 -0500

    create playRound function

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 Rock Paper Scissors game from Project Odin lesson
 
 User plays against the computer.
+
+Plan:
+    - create getComputerChoice function that randomly returns 'rock', 'paper', or 'scissors'
+
+    - get user input 
+
+    - compare user input with computer choice to determine the winner
+
+    - display winner of the round
--- rps.js
+++ rps.js
@@ -1,23 +1,46 @@
 /* declare variables */
 let computerChoice = '';
-let playerChoice = '';
+const playerSelection = 'rock';
 
 /* gets computer choice using random number 1 through 3 and returns to variable computerSelection*/
 function getComputerChoice () {
    const rdm = Math.floor(Math.random() * 3) + 1;
    switch (rdm) {
     case 1:
-        computerChoice = 'Rock';
+        computerChoice = 'rock';
         break;
     case 2:
-        computerChoice = 'Paper';
+        computerChoice = 'paper';
         break;
     case 3:
-        computerChoice = 'Scissors';
+        computerChoice = 'scissors';
         break;
    }
-   console.log(computerChoice);
+   return computerChoice;
 }
-/* gets user choice via console input and returns to variable playerSelection */
 /* plays a round of Rock Paper Scissors and returns a string declaring the winner */
-
+function playRound (computerChoice, playerSelection) {
+    let winner = '';
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerChoice) {
+        winner = 'It\'s a tie!';
+    } else if (playerSelection == 'rock' && computerChoice == 'paper') {
+        winner = 'You lose!! Paper covers rock.';
+    } else if (playerSelection == 'rock' && computerChoice == 'scissors') {
+        winner = 'You win!! Rock smashes scissors.';
+    } else if (playerSelection == 'paper' && computerChoice == 'rock') {
+        winner = 'You win!! Paper covers rock.';
+    } else if (playerSelection == 'paper' && computerChoice == 'scissors') {
+        winner = 'You lose!! Scissors cut paper.';
+    } else if (playerSelection == 'scissors' && computerChoice == 'rock') {
+        winner = 'You lose!! Rock smashes scissors';
+    } else if (playerSelection == 'scissors' && computerChoice == 'paper') {
+        winner = 'You win!! Scissors cut paper.';
+    } else {
+        winner = 'You didn\'t enter a valid choice, try again!';
+    }
+    return winner;
+}
+getComputerChoice();
+console.log(playRound (computerChoice, playerSelection));
+console.log('Computer choice: ' + computerChoice + '    Player choice: ' + playerSelection);
